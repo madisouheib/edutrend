eb71c76457ba1015b941b3ad5f88fc3d358d8107
souheibmadi <sm@aimngt.com>
2022-09-01 10:27:23 +0100

    first commit new links ver 8

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -38,7 +38,9 @@
   <!--HOVER CSS LIBRARY-->
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/hover.css/2.3.1/css/hover-min.css" integrity="sha512-csw0Ma4oXCAgd/d4nTcpoEoz4nYvvnk21a8VA2h2dzhPAvjbUIK6V3si7/g/HehwdunqqW18RwCJKpD7rL67Xg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
  <!-- FLICKITY SWIPER-->
-
+    <!--swiper in testimonials  -->
+    <link rel="stylesheet" href="https://unpkg.com/swiper@8/swiper-bundle.min.css">
+    <script src="https://unpkg.com/swiper@8/swiper-bundle.min.js"></script>
 
 
 
