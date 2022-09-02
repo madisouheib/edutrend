6c8ef0230724660c93328245a7254e8bfed0d9db
souheibmadi <sm@aimngt.com>
2022-09-02 16:23:16 +0100

    first commit new links ver top

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -15,8 +15,8 @@
                 <!-- video js  -->
                 @if(!empty($home->vedio_link))
 
-                <iframe class="video-js vjs-theme-sea"  allowpaymentrequest src="{{ $home->vedio_link }}"  title="Iframe Example"></iframe>
-           
+                <iframe class="video-js vjs-theme-sea"  style="border-radius: 10px;"  src="{{ $home->vedio_link }}"  title="Iframe Example"></iframe>
+
                 @else 
                 <img src="{{ Voyager::image( $home->img_link  ) }}"  class="img-responsive" style="border-radius: 10px;">
                 @endif
