ff0a07a9d4d7362237305f3a7234d4c2f9f78860
souheibmadi <sm@aimngt.com>
2022-09-04 15:37:14 +0100

    first commit new   fix new  update v5dsdsd

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -113,7 +113,7 @@
           <!-- side image -->
           <div class="col-12 col-md-5 col-lg-6">
             <div class="lc-block">
-              <iframe class="video-js vjs-theme-sea"  style="border-radius: 10px;height:580px;width:420px"  src="{{ $home->second_vedio }}"  title="Iframe Example"></iframe>
+              <iframe class="video-js vjs-theme-sea"  style="border-radius: 10px;height:300px;width:420px"  src="{{ $home->second_vedio }}"  title="Iframe Example"></iframe>
             </div>
             <!-- /lc-block -->
           </div>
